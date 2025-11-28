42bb81dfce478e380293b422089dc3617c47e003
himanshudubey <hackerhimanshudubey7778@gmail.com>
2025-11-28 14:51:22 +0530

    first commit add

---
+++ 01_Basic.js/01_Variable.js
@@ -0,0 +1,21 @@
+
+   //+++++ var ++++++
+var name = "himanhsu";
+name = "Pizza Aur Code";
+// console.log(name);
+
+     //+++let++++++
+let age = 20;
+age = 21;
+// console.log(age);
+
+// +++++ const ++++++
+const pi = 3.14;   // allowed 
+// pi = 2.12;     // not allowed 
+// console.log(pi);
+
+
+console.table([name, age, pi]);
+
+
+
